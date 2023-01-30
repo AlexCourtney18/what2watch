80ee436db15d9eacf9de3fb040b23efa7c392565
Alex <lex.courtney18@gmail.com>
2023-01-30 11:19:30 -0600

    add basic validation to signup form

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,13 +1,37 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function SignupForm() {
 
     const [formState, setFormState] = useState({ name: '', email: '', password: '' });
 
+    const [errorMessage, setErrorMessage] = useState('');
+
     const { name, email, password } = formState;
 
     function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+
+        if (!errorMessage) {
         setFormState({...formState, [e.target.name]: e.target.value })
+        }
+
+        console.log('errorMessage', errorMessage);
     }
 
     function handleSubmit(e) {
